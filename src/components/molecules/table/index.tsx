736e0b3978bec7efa2318e286d1407f7c421ffa9
Thachdong <thachdong270293@gmail.com>
2025-01-20 16:26:22 +0700

    feat(base-components): add component - table

---
+++ src/components/molecules/table/index.tsx
@@ -0,0 +1,103 @@
+import React, { useMemo } from "react";
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+  RowData,
+} from "@tanstack/react-table";
+import { TTableProps } from "@/types/table";
+import { joinClass } from "@/helpers/style";
+import style from "./table.module.scss";
+
+const Table = <T extends RowData>({
+  data,
+  columns,
+  containerClassName,
+  tableclassName,
+  theadClassName,
+  tbodyClassName,
+  tfootClassName,
+  showFooter,
+  borderless,
+}: TTableProps<T>) => {
+  const reactTable = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  const tableHead = useMemo(() => {
+    const headerGroups = reactTable.getHeaderGroups();
+
+    console.log(headerGroups, headerGroups.length);
+
+    if (headerGroups.length === 0) return <></>;
+
+    return headerGroups.map((headerGroup) => (
+      <tr key={headerGroup.id}>
+        {headerGroup.headers.map((header) => (
+          <th key={header.id}>
+            {header.isPlaceholder
+              ? null
+              : flexRender(header.column.columnDef.header, header.getContext())}
+          </th>
+        ))}
+      </tr>
+    ));
+  }, [reactTable]);
+
+  const tableBody = useMemo(() => {
+    const rows = reactTable.getRowModel().rows;
+
+    console.log(rows, rows.length);
+
+    if (rows.length === 0) return <></>;
+
+    return rows.map((row) => (
+      <tr key={row.id}>
+        {row.getVisibleCells().map((cell) => (
+          <td key={cell.id}>
+            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+          </td>
+        ))}
+      </tr>
+    ));
+  }, [reactTable]);
+
+  const tableFooter = useMemo(() => {
+    const footerGroups = reactTable.getFooterGroups();
+
+    return footerGroups.map((footerGroup) => (
+      <tr key={footerGroup.id}>
+        {footerGroup.headers.map((header) => (
+          <th key={header.id}>
+            {header.isPlaceholder
+              ? null
+              : flexRender(header.column.columnDef.footer, header.getContext())}
+          </th>
+        ))}
+      </tr>
+    ));
+  }, [reactTable]);
+
+  return (
+    <div
+      className={joinClass(
+        containerClassName,
+        !borderless ? style["table-border"] : ""
+      )}
+    >
+      <table className={joinClass(tableclassName, "w-full")}>
+        <thead className={joinClass(theadClassName)}>{tableHead}</thead>
+
+        <tbody className={joinClass(tbodyClassName)}>{tableBody}</tbody>
+
+        {showFooter && (
+          <tfoot className={joinClass(tfootClassName)}>{tableFooter}</tfoot>
+        )}
+      </table>
+    </div>
+  );
+};
+
+export default Table;
