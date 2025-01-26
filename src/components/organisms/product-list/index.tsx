18ef9aca6f85df03009f8cc937420c8c150d6529
Thachdong <thachdong270293@gmail.com>
2025-01-26 22:07:47 +0700

    feat(base-components): add component - ProductList

---
+++ src/components/organisms/product-list/index.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/atoms/button";
+import { Header } from "@/components/atoms/header";
+import { ProductCard } from "@/components/molecules/product-card";
+import { EButtonType } from "@/constants";
+import { joinClass } from "@/helpers/style";
+import React from "react";
+
+const CONTAINER_CLASSNAME = "flex flex-col gap-y-2 P-4";
+
+const LIST_CLASSNAME =
+  "grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4";
+
+const BUTTON_CLASSNAME = "self-center";
+
+export const ProductList: React.FC<Readonly<TProductList>> = ({
+  title,
+  className,
+  listClassName,
+  products,
+  loadMoreAction,
+}) => {
+  return (
+    <div className={joinClass(CONTAINER_CLASSNAME, className)}>
+      <Header level={3}>{title}</Header>
+
+      <div className={joinClass(LIST_CLASSNAME, listClassName)}>
+        {products.map((product) => (
+          <ProductCard key={product.product.id} product={product.product} />
+        ))}
+      </div>
+
+      <Button
+        className={BUTTON_CLASSNAME}
+        variant={EButtonType.outline}
+        onClick={loadMoreAction}
+      >
+        Load more
+      </Button>
+    </div>
+  );
+};
